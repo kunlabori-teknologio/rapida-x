81bca84539c9c8e8a98718327eb8b73b3277d8d5
Alexis Barros <apenasalexis@gmail.com>
2023-10-05 00:26:26 -0300

    :art: add install nvm step

--- src/controller/frontend/angular/01-make-skeleton-object.js
+++ src/controller/frontend/angular/01-make-skeleton-object.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const chp = require('child_process');
-const projectsPath = path.join(__dirname, '..', '..', '..', '..', 'project');
-const appsPath = path.join(__dirname, '..', '..', '..', '..', 'apps');
+const path = require("path");
+const chp = require("child_process");
+const projectsPath = path.join(__dirname, "..", "..", "..", "..", "project");
+const appsPath = path.join(__dirname, "..", "..", "..", "..", "apps");
 
 const makeAngularSkeleton = async (array) => {
   const project = array[0].project;
-  
+
   console.info(`Check project folder existence`);
   try {
     chp.execSync(`[ -d "${appsPath}/${project.folder}" ]`);
@@ -14,12 +14,25 @@ const makeAngularSkeleton = async (array) => {
     return true;
   } catch (err) {
     try {
+      console.info(`Install nvm`);
+      chp.execSync(
+        "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash"
+      );
+      chp.execSync(
+        'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"'
+      );
+      chp.execSync('[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"');
+
       console.info(`Install node lts`);
       chp.execSync('/bin/bash -c "source ~/.nvm/nvm.sh && nvm install --lts"');
       console.info(`Install angular lts`);
-      chp.execSync('npm uninstall -g @angular/cli && npm cache clean --force && npm cache verify && npm install -g @angular/cli');
+      chp.execSync(
+        "npm uninstall -g @angular/cli && npm cache clean --force && npm cache verify && npm install -g @angular/cli"
+      );
       console.info(`Create new angular app "${project.folder}"`);
-      chp.execSync(`ng new ${project.folder} --directory apps/${project.folder} --routing --style=scss`);
+      chp.execSync(
+        `ng new ${project.folder} --directory apps/${project.folder} --routing --style=scss`
+      );
       console.info(`${projectsPath}/${project.folder} created`);
       console.info(`Create components folder`);
       chp.execSync(`mkdir ${appsPath}/${project.folder}/src/app/components`);
@@ -33,8 +46,8 @@ const makeAngularSkeleton = async (array) => {
       return error.message;
     }
   }
-}
+};
 
 module.exports = {
-  makeAngularSkeleton
-}
+  makeAngularSkeleton,
+};
