238170af1a53602a1e8963698037e3729bc0f62f
Alexis Barros <apenasalexis@gmail.com>
2023-10-05 00:29:00 -0300

    :construction: WIP backend generate code

--- src/controller/backend/index.js
+++ src/controller/backend/index.js
@@ -1,14 +1,43 @@
-const {startLoopbackCoding} = require("./loopback/index");
+const { makeLoopbackSkeleton } = require("./loopback/01-make-skeleton-object");
+// const { makeAngularCode } = require("./angular/02-make-code-object");
+// const {
+//   makeAngularFiles,
+// } = require("./angular/03-make-files-array_of_objects");
+// const {
+//   matchAngularFilesAndCodes,
+// } = require("./angular/04-match-files_and_codes");
 
-const loopback = async (project) => {
-  await startLoopbackCoding(project);  
-}
+let code = "";
 
-const gin = (project) => {
-  
-}
+const createBackendFramework = async (array) => {
+  switch (array[0].project.backendFramework) {
+    case "loopback":
+      loopback(array);
+      break;
+
+    case "nest":
+      break;
+
+    case "go":
+      break;
+
+    default:
+      break;
+  }
+};
+
+const loopback = async (array) => {
+  await makeLoopbackSkeleton(array);
+  // const code = await makeAngularCode(array);
+  // makeAngularFiles(code);
+  // matchAngularFilesAndCodes(code);
+  return false;
+};
+
+const nest = (array) => {};
+
+const go = (array) => {};
 
 module.exports = {
-	loopback,
-	gin
-}
+  createBackendFramework,
+};
